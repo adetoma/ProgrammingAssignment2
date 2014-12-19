bff9050092e278a8320bc5ba190c1bacb3f26414
adetoma <detoma@musc.edu>
2014-12-19 11:14:58 -0500

    added my R code to the ProgrammingAssingment2 file

---
+++ program2detoma.R
@@ -0,0 +1,32 @@
+##This function creates a special "matrix" object that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {  
+        m <- NULL                    
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+
+##This function computes the inverse of the special "matrix" returned by the 
+##makeCacheMatrix function. If the inverse has already been calculated (and the
+##matrix has not changed), then this function should retrieve the inverse from 
+##the cache.
+
+cacheSolve <- function(x,...) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        mat <- x$get()
+        m <- solve(mat,...)
+        x$setinverse(m)
+        m
+}
